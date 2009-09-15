6a1739843c06bde76a14165f45ffdf13a6018004
Jonathan <rookieone@gmail.com>
2009-09-14 23:22:22 -0500

    Working on Validation Rules

---
+++ WpfApplication/ValidationRules/GreaterThanRule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using WpfApplication.Framework;
+
+namespace WpfApplication.ValidationRules
+{
+    public class GreaterThanRule : ValidationRule
+    {
+        private readonly int _number;
+
+        public GreaterThanRule(int number)
+        {
+            _number = number;
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value == null)
+                return new ValidationResult(false, "Must be a number");
+
+            int v;
+            if (!Int32.TryParse(value.ToString(), out v))
+                return new ValidationResult(false, "Must be a number");
+
+            return v > _number
+                       ? new ValidationResult(true, null)
+                       : new ValidationResult(false, "{0} must be greater than {1}".FormatString(v, _number));
+        }
+    }
+}
